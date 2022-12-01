ef89cfbf9d22e0f89ab8eccaf5cbf4d44b1fa730
MarioOlla <mario.olla25@gmail.com>
2022-12-01 14:05:52 +0100

    primo commit

---
+++ src/it/prova/conflitti/Test.java
@@ -0,0 +1,12 @@
+package it.prova.conflitti;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Ecco il main di conflitti");
+		
+		System.out.println("questi conflitti sono orribili");
+		
+		System.out.println("proprio orribili");
+	}
+}
